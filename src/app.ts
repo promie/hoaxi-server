e178fe8ad7632e2ab617270f9855d1da144f17c0
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-21 07:06:34 +1100

    Linking up route and fixing integration test

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import httpStatus from 'http-status'
+import { UserRoute } from '../routes'
 
 const app: Application = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/api/1.0/users', UserRoute)
 
 // Handling Unhandled Routes
 app.all('*', (req: Request, res: Response, _next: NextFunction) => {
