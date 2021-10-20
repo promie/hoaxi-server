2de839cdcb0b4ebfb110d193e3529394fa626214
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-21 06:28:30 +1100

    Switching project to run on port 3000 and added test:watch script

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from './app'
 import http from 'http'
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const startServer = async (): Promise<http.Server> => {
   return app.listen(PORT, () => {
