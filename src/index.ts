cf8f99835123d1b3cf6b40e9a6b6f0a8c90e0e9d
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-22 16:39:22 +1100

    initalising database on the main app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import app from './app'
 import http from 'http'
+import sequelize from './config/database'
 import { Logger } from './utils'
 
 const PORT = process.env.PORT || 3000
+
+sequelize.sync()
 
 const startServer = async (): Promise<http.Server> => {
   return app.listen(PORT, () => {
