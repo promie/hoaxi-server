34f7d01b737aa65f60f3393d270e05e331b592dc
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-24 07:25:12 +1100

    test refactor to use a common postValidUser function

--- src/app.int.test.ts
+++ src/app.int.test.ts
@@ -13,33 +13,28 @@ beforeEach(() => {
 
 describe('Integration Tests', () => {
   describe('User Registration', () => {
-    it('returns 200 OK when signup request is valid', async () => {
-      await request(app)
-        .post('/api/1.0/users')
-        .send({
-          username: 'user1',
-          email: 'user1@mail.com',
-          password: 'P4ssword',
-        })
-        .expect(200)
-    })
-
-    it('returns success message when signup request is valid', async () => {
-      const response = await request(app).post('/api/1.0/users').send({
+    const postValidUser = () => {
+      return request(app).post('/api/1.0/users').send({
         username: 'user1',
         email: 'user1@mail.com',
         password: 'P4ssword',
       })
+    }
+
+    it('returns 200 OK when signup request is valid', async () => {
+      const response = await postValidUser()
+
+      expect(response.status).toBe(200)
+    })
+
+    it('returns success message when signup request is valid', async () => {
+      const response = await postValidUser()
 
       expect(response.body.message).toBe('User created')
     })
 
     it('saves the user to database', async () => {
-      await request(app).post('/api/1.0/users').send({
-        username: 'user1',
-        email: 'user1@mail.com',
-        password: 'P4ssword',
-      })
+      await postValidUser()
 
       const userList = await User.findAll()
 
@@ -47,11 +42,7 @@ describe('Integration Tests', () => {
     })
 
     it('saves the username and email to database', async () => {
-      await request(app).post('/api/1.0/users').send({
-        username: 'user1',
-        email: 'user1@mail.com',
-        password: 'P4ssword',
-      })
+      await postValidUser()
 
       const userList = await User.findAll()
       const savedUser = userList[0]
@@ -63,11 +54,7 @@ describe('Integration Tests', () => {
     })
 
     it('hashes the password in the database', async () => {
-      await request(app).post('/api/1.0/users').send({
-        username: 'user1',
-        email: 'user1@mail.com',
-        password: 'P4ssword',
-      })
+      await postValidUser()
 
       const userList = await User.findAll()
       const savedUser = userList[0]
