e8794a9c939a32c15d36f5561831ec34b4aaf52b
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-24 07:48:34 +1100

    added configuration files for development and test databases

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,18 @@
+// @ts-nocheck
 import { Sequelize } from 'sequelize'
+import config from 'config'
 
-const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-})
+const dbConfig = config.get('database')
+
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    dialect: dbConfig.dialect,
+    storage: dbConfig.storage,
+    logging: dbConfig.logging,
+  },
+)
 
 export default sequelize
