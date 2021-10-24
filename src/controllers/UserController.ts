13b9180a6caa997c60ae4168ce00ad6d3762d7f4
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-25 09:33:34 +1100

    implementation to make the tests pass

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,7 +3,12 @@ import httpStatus from 'http-status'
 import { UserService } from '../services'
 
 const signUp = async (req: Request, res: Response, _next: NextFunction) => {
-  await UserService.signUp(req.body)
+  const user = await UserService.signUp(req.body)
+
+  // @ts-ignore
+  if (user.username === null) {
+    return res.status(httpStatus.BAD_REQUEST).send()
+  }
 
   return res.status(httpStatus.OK).json({
     message: 'User created',
