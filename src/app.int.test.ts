5d133cbc6147d0fa1d762202e15759fe06d27cff
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-27 06:58:48 +1100

    refactor to dynamic tests for validations

--- src/app.int.test.ts
+++ src/app.int.test.ts
@@ -93,30 +93,6 @@ describe('Integration Tests', () => {
       expect(body.validationErrors).not.toBeUndefined()
     })
 
-    it('returns Username cannot be null when username is null', async () => {
-      const response = await postUser({
-        username: null,
-        email: 'user1@mail.com',
-        password: 'P4ssword',
-      })
-
-      const body = response.body
-
-      expect(body.validationErrors.username).toBe('Username cannot be null')
-    })
-
-    it('returns Email cannot be null when email is null', async () => {
-      const response = await postUser({
-        username: 'user1',
-        email: null,
-        password: 'P4ssword',
-      })
-
-      const body = response.body
-
-      expect(body.validationErrors.email).toBe('E-mail cannot be null')
-    })
-
     it('returns errors for both when  username and email is null', async () => {
       const response = await postUser({
         username: null,
@@ -129,16 +105,22 @@ describe('Integration Tests', () => {
       expect(Object.keys(body.validationErrors)).toEqual(['username', 'email'])
     })
 
-    it('returns Password cannot be null when password is null', async () => {
-      const response = await postUser({
+    it.each([
+      ['username', 'Username cannot be null'],
+      ['email', 'E-mail cannot be null'],
+      ['password', 'Password cannot be null'],
+    ])('when %s is null %s is received', async (field, expectedMessage) => {
+      const user = {
         username: 'user1',
         email: 'user1@mail.com',
-        password: null,
-      })
+        password: 'P4ssword',
+      }
 
+      // @ts-ignore
+      user[field] = null
+      const response = await postUser(user)
       const body = response.body
-
-      expect(body.validationErrors.password).toBe('Password cannot be null')
+      expect(body.validationErrors[field]).toBe(expectedMessage)
     })
   })
 })
