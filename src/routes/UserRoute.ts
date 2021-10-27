f0016a2b59681a73c7405694964ba1c5394fe54b
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-28 07:23:26 +1100

    implementation for the tests to pass for validation messages

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -7,7 +7,12 @@ const router = express.Router()
 
 router.post(
   '/',
-  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    .bail()
+    .isLength({ min: 4 })
+    .withMessage('Must have min 4 and max 32 characters'),
   check('email').notEmpty().withMessage('E-mail cannot be null'),
   check('password').notEmpty().withMessage('Password cannot be null'),
   signUpValidationMiddleware,
